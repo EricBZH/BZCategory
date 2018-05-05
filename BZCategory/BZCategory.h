4cf336f77a1897cf46a8140a324006ef2240ed28
EricBZH <38752174+EricBZH@users.noreply.github.com>
2018-05-05 17:17:38 +0800

    1.添加项目文件。

---
+++ BZCategory/BZCategory.h
@@ -0,0 +1,19 @@
+//
+//  BZCategory.h
+//  BZCategory
+//
+//  Created by Eric on 2018/5/5.
+//  Copyright © 2018年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BZCategory.
+FOUNDATION_EXPORT double BZCategoryVersionNumber;
+
+//! Project version string for BZCategory.
+FOUNDATION_EXPORT const unsigned char BZCategoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BZCategory/PublicHeader.h>
+
+
---
+++ BZCategoryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BZCategoryDemo
+//
+//  Created by Eric on 2018/5/5.
+//  Copyright © 2018年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
