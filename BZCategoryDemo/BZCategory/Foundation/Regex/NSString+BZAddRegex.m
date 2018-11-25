29b71f4ef15c9a9a71e81767765867ae262bdbe2
毕志豪 <eric__b@163.com>
2018-11-25 17:32:13 +0800

    “1.修改pod版本号为1.3.4。2.修改邮箱校验正则。”

--- BZCategoryDemo/BZCategory/Foundation/Regex/NSString+BZAddRegex.m
+++ BZCategoryDemo/BZCategory/Foundation/Regex/NSString+BZAddRegex.m
@@ -17,7 +17,7 @@
  */
 - (BOOL)bz_isValidEmail
 {
-    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSString *emailRegex = @"^[A-Za-z\\d-_.]+@[a-zA-Z\\d-_]+(\\.[a-zA-Z\\d-_]+)+$";
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
     return [emailTest evaluateWithObject:self];
 }
